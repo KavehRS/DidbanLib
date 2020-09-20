357abf7d9a6eada769531e045a2f99b699811e4d
Kaveh Rezaeishiraz <me@kavehrs.com>
2020-09-20 01:42:22 -0400

    Update DidbanLib.js

--- DidbanLib.js
+++ DidbanLib.js
@@ -1,1 +1,115 @@
-var active_session,ip,user_id,timeout=1,url="http://192.168.143.18:8876/api/",system_id="Developer",auth_token="eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJSYXNhZE1pbGFkIiwic3ViIjoiRGV2ZWxvcGVyIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl19.P60tgVqx8lXcX2RFLGqlPe0BLWW5MlQL34LK9UTfqKPcl5PoY1w7ttYzPWw16Zr9oAOXi-dKeU4jseVO_u8ljg",ttl=30,counter=ttl,ACTIVITY={Play:1,Pause:2,FDStart:3,FDEnd:4,BDStart:5,BDEnd:6,ContentView:7},SERVICE_TYPE={Live:1,TimeShift:2,CatchUp:3,OnDemand:4},CONTENT_TYPE={Video:1,Audio:2,Image:3,Text:4};function getUserIP(e){var t=new(window.RTCPeerConnection||window.mozRTCPeerConnection||window.webkitRTCPeerConnection)({iceServers:[]}),n=function(){},o={},i=/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g;function r(t){o[t]||"0.0.0.0"==t||e(t),ipFound=!0}ipFound=!1,t.createDataChannel(""),t.createOffer().then(function(e){e.sdp.split("\n").forEach(function(e){ipFound&&exit,e.indexOf("IP4")<0||e.match(i).forEach(r)}),t.setLocalDescription(e,n,n)}).catch(function(e){}),t.onicecandidate=function(e){e&&e.candidate&&e.candidate.candidate&&e.candidate.candidate.match(i)&&e.candidate.candidate.match(i).forEach(r)}}function getCookie(e){e+="=";for(var t=decodeURIComponent(document.cookie).split(";"),n=0;n<t.length;n++){for(var o=t[n];" "==o.charAt(0);)o=o.substring(1);if(0==o.indexOf(e))return o.substring(e.length,o.length)}return""}function setCookie(e,t){if(t){var n=new Date;n.setMinutes(n.getMinutes()+timeout),document.cookie="{0}={1}; expires={2}".format(e,t,n.toUTCString())}else document.cookie="{0}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;".format(e)}function create_UUID(){var e=(new Date).getTime();return"xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g,function(t){var n=(e+16*Math.random())%16|0;return e=Math.floor(e/16),("x"==t?n:3&n|8).toString(16)})}String.prototype.format||(String.prototype.format=function(){var e=arguments;return this.replace(/{(\d+)}/g,function(t,n){return void 0!==e[n]?e[n]:t})}),getUserIP(function(e){ip=e}),sessionFactory={check:function(){var e=getCookie("token");return e?(active_session=e,console.log("Session is already opened. Token {0}".format(e))):sessionFactory.init(user_id),!0},init:function(e){if(ip){var t=getCookie("token");if(user_id!=e||!t){t=create_UUID();user_id=null!=e?e:t,setCookie("token",t),user_agent=navigator.userAgent,referer=document.location.origin,xReferer=document.location.origin;var n='{"sys_id": "{0}", "user_id": "{1}", "session_id": "{2}", "ip": "{3}","user_agent": "{4}", "referer": "{5}", "xReferer": "{6}"}'.format(system_id,user_id,t,ip,user_agent,referer,xReferer),o=new XMLHttpRequest;return o.open("POST","{0}session/".format(url),!0),o.setRequestHeader("Content-Type","application/json"),o.setRequestHeader("Authorization",window.btoa(auth_token)),o.onreadystatechange=function(){4==this.readyState&&201==this.status?console.log("Success: {0}: {1}".format(this.status,this.responseText)):console.log("Error: {0}: {1}".format(this.status,this.responseText))},o.send(n),!0}setCookie("token",t)}else setTimeout(function(){0!=counter--?sessionFactory.init(user_id):counter=ttl},1e3)},expire:function(){return setCookie("token",null),user_id=null,!0}},activityFactory={log:function(e,t,n,o,i,r){sessionFactory.check();var a=getCookie("token"),s='{"session_id": "{0}", "channel_id": "{1}", "content_id": "{2}","content_type_id": "{3}", "service_id": "{4}","action_id": "{5}", "time_code": "{6}"}'.format(a,e,t,n,o,i,r),c=new XMLHttpRequest;return c.open("POST","{0}event/".format(url),!0),c.setRequestHeader("Content-Type","application/json"),c.setRequestHeader("Authorization",window.btoa(auth_token)),c.onreadystatechange=function(){4==this.readyState&&201==this.status?(setCookie("token",a),console.log("Token {0} did activity {1}".format(a,i))):console.log("Activity logging failed.")},c.send(s),!0}};
+var active_session, ip, user_id, timeout = 1,
+    url = "http://192.168.143.18:8876/api/",
+    system_id = "Developer",
+    auth_token = "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJSYXNhZE1pbGFkIiwic3ViIjoiRGV2ZWxvcGVyIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl19.P60tgVqx8lXcX2RFLGqlPe0BLWW5MlQL34LK9UTfqKPcl5PoY1w7ttYzPWw16Zr9oAOXi-dKeU4jseVO_u8ljg",
+    ttl = 30,
+    counter = ttl,
+    ACTIVITY = {
+        Play: 1,
+        Pause: 2,
+        FDStart: 3,
+        FDEnd: 4,
+        BDStart: 5,
+        BDEnd: 6,
+        ContentView: 7
+    },
+    SERVICE_TYPE = {
+        Live: 1,
+        TimeShift: 2,
+        CatchUp: 3,
+        OnDemand: 4
+    },
+    CONTENT_TYPE = {
+        Video: 1,
+        Audio: 2,
+        Image: 3,
+        Text: 4
+    };
+
+function getUserIP(e) {
+    var t = new(window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection)({
+            iceServers: []
+        }),
+        n = function() {},
+        o = {},
+        i = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g;
+
+    function r(t) {
+        o[t] || "0.0.0.0" == t || e(t), ipFound = !0
+    }
+    ipFound = !1, t.createDataChannel(""), t.createOffer().then(function(e) {
+        e.sdp.split("\n").forEach(function(e) {
+            ipFound && exit, e.indexOf("IP4") < 0 || e.match(i).forEach(r)
+        }), t.setLocalDescription(e, n, n)
+    }).catch(function(e) {}), t.onicecandidate = function(e) {
+        e && e.candidate && e.candidate.candidate && e.candidate.candidate.match(i) && e.candidate.candidate.match(i).forEach(r)
+    }
+}
+
+function getCookie(e) {
+    e += "=";
+    for (var t = decodeURIComponent(document.cookie).split(";"), n = 0; n < t.length; n++) {
+        for (var o = t[n];
+            " " == o.charAt(0);) o = o.substring(1);
+        if (0 == o.indexOf(e)) return o.substring(e.length, o.length)
+    }
+    return ""
+}
+
+function setCookie(e, t) {
+    if (t) {
+        var n = new Date;
+        n.setMinutes(n.getMinutes() + timeout), document.cookie = "{0}={1}; expires={2}".format(e, t, n.toUTCString())
+    } else document.cookie = "{0}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;".format(e)
+}
+
+function create_UUID() {
+    var e = (new Date).getTime();
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(t) {
+        var n = (e + 16 * Math.random()) % 16 | 0;
+        return e = Math.floor(e / 16), ("x" == t ? n : 3 & n | 8).toString(16)
+    })
+}
+String.prototype.format || (String.prototype.format = function() {
+    var e = arguments;
+    return this.replace(/{(\d+)}/g, function(t, n) {
+        return void 0 !== e[n] ? e[n] : t
+    })
+}), getUserIP(function(e) {
+    ip = e
+}), sessionFactory = {
+    check: function() {
+        var e = getCookie("token");
+        return e ? (active_session = e, console.log("Session is already opened. Token {0}".format(e))) : sessionFactory.init(user_id), !0
+    },
+    init: function(e) {
+        if (ip) {
+            var t = getCookie("token");
+            if (user_id != e || !t) {
+                t = create_UUID();
+                user_id = null != e ? e : t, setCookie("token", t), user_agent = navigator.userAgent, referer = document.location.origin, xReferer = document.location.origin;
+                var n = '{"sys_id": "{0}", "user_id": "{1}", "session_id": "{2}", "ip": "{3}","user_agent": "{4}", "referer": "{5}", "xReferer": "{6}"}'.format(system_id, user_id, t, ip, user_agent, referer, xReferer),
+                    o = new XMLHttpRequest;
+                return o.open("POST", "{0}session/".format(url), !0), o.setRequestHeader("Content-Type", "application/json"), o.setRequestHeader("Authorization", window.btoa(auth_token)), o.onreadystatechange = function() {
+                    4 == this.readyState && 201 == this.status ? console.log("Success: {0}: {1}".format(this.status, this.responseText)) : console.log("Error: {0}: {1}".format(this.status, this.responseText))
+                }, o.send(n), !0
+            }
+            setCookie("token", t)
+        } else setTimeout(function() {
+            0 != counter-- ? sessionFactory.init(user_id) : counter = ttl
+        }, 1e3)
+    },
+    expire: function() {
+        return setCookie("token", null), user_id = null, !0
+    }
+}, activityFactory = {
+    log: function(e, t, n, o, i, r) {
+        sessionFactory.check();
+        var a = getCookie("token"),
+            s = '{"session_id": "{0}", "channel_id": "{1}", "content_id": "{2}","content_type_id": "{3}", "service_id": "{4}","action_id": "{5}", "time_code": "{6}"}'.format(a, e, t, n, o, i, r),
+            c = new XMLHttpRequest;
+        return c.open("POST", "{0}event/".format(url), !0), c.setRequestHeader("Content-Type", "application/json"), c.setRequestHeader("Authorization", window.btoa(auth_token)), c.onreadystatechange = function() {
+            4 == this.readyState && 201 == this.status ? (setCookie("token", a), console.log("Token {0} did activity {1}".format(a, i))) : console.log("Activity logging failed.")
+        }, c.send(s), !0
+    }
+};
