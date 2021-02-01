fcab73ff1cf27fce21916ff34170d034b5b9f22f
Kaveh RezaeiShiraz <kavehrs@pm.me>
2021-02-01 09:56:40 +0330

    Update Didban.js

--- Didban.js
+++ Didban.js
@@ -1,11 +1,9 @@
 /****************************************************************
  PROGRAM:   Didban Lib
  AUTHOR:    Ali Emami, Kaveh Rezaei Shiraz, Maryam Mostafa Nazai Zanjani.
- LOGON ID (git.irib.ir):    a.emami, kavehrs, Nazari
  DUE DATE:  11/14/2020
  Version : 2.0.0
  FUNCTION:  Get Data From Cleint side and send for IRIB Analytic Server
-
  INPUT:     ACTIVITY, SERVICE_TYPE, CONTENT_TYPE
  ****************************************************************/
 
